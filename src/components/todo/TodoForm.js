54aa9cfe9290e21c47b62533f58504e009074b9f
Ben Muschol <benmuschol@gmail.com>
2017-04-04 13:07:14 -0400

    lesson 6: propTypes

--- src/components/todo/TodoForm.js
+++ src/components/todo/TodoForm.js
@@ -4,3 +4,8 @@ export const TodoForm = (props) => (
   <form>
     <input type="text" onChange={props.handleInputChange} value={props.currentTodo}/>
   </form>)
+
+TodoForm.propTypes = {
+  currentTodo: React.PropTypes.string.isRequired,
+  handleInputChange: React.PropTypes.func.isRequired
+}
