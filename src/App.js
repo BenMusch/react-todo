7f1942499272d54305aae31ac2b398621d269c8a
Ben Muschol <benmuschol@gmail.com>
2017-04-05 19:18:50 -0400

    lesson 9: updating todo list with form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {TodoForm, TodoList} from './components/todo'
+import {addTodo, generateId} from './lib/todoHelpers'
 
 class App extends Component {
   constructor() {
@@ -15,6 +16,15 @@ class App extends Component {
       currentTodo: ''
     }
     this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit (event) {
+    event.preventDefault()
+    const newId = generateId()
+    const newTodo = { id: newId, name: this.state.currentTodo, isComplete: false }
+    const updatedTodos = addTodo(this.state.todos, newTodo)
+    this.setState({ todos: updatedTodos, currentTodo: '' })
   }
 
   handleInputChange (event) {
@@ -31,7 +41,9 @@ class App extends Component {
           <h2>React Todos</h2>
         </div>
         <div className="Todo-App">
-          <TodoForm handleInputChange={this.handleInputChange} currentTodo={this.state.currentTodo} />
+          <TodoForm handleInputChange={this.handleInputChange}
+             currentTodo={this.state.currentTodo}
+             handleSubmit={this.handleSubmit} />
           <TodoList todos={this.state.todos} />
         </div>
       </div>
