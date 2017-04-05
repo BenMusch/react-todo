5b00359d13650984d07dc7f78f6b1fe1ea85784c
Ben Muschol <benmuschol@gmail.com>
2017-04-05 19:51:35 -0400

    lesson 10: form validation

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
     }
     this.handleInputChange = this.handleInputChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleEmptySubmit = this.handleEmptySubmit.bind(this)
   }
 
   handleSubmit (event) {
@@ -24,7 +25,18 @@ class App extends Component {
     const newId = generateId()
     const newTodo = { id: newId, name: this.state.currentTodo, isComplete: false }
     const updatedTodos = addTodo(this.state.todos, newTodo)
-    this.setState({ todos: updatedTodos, currentTodo: '' })
+    this.setState({
+      todos: updatedTodos,
+      currentTodo: '',
+      errorMessage: ''
+    })
+  }
+
+  handleEmptySubmit (event) {
+    event.preventDefault();
+    this.setState({
+      errorMessage: 'Please supply a todo name'
+    })
   }
 
   handleInputChange (event) {
@@ -34,6 +46,7 @@ class App extends Component {
   }
 
   render() {
+    const submitHandler = this.state.currentTodo ? this.handleSubmit : this.handleEmptySubmit
     return (
       <div className="App">
         <div className="App-header">
@@ -41,9 +54,10 @@ class App extends Component {
           <h2>React Todos</h2>
         </div>
         <div className="Todo-App">
+          {this.state.errorMessage && <span className='error'>{this.state.errorMessage}</span>}
           <TodoForm handleInputChange={this.handleInputChange}
              currentTodo={this.state.currentTodo}
-             handleSubmit={this.handleSubmit} />
+             handleSubmit={submitHandler} />
           <TodoList todos={this.state.todos} />
         </div>
       </div>
