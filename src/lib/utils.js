97f8d8ccad951c867570d488a7769a5638248d78
Ben Muschol <benmuschol@gmail.com>
2017-04-06 21:42:34 -0400

    lesson 14: function composition

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,1 +1,5 @@
-export const partial = (func, ...args) => func.bind(null, ...args)
+export const partial = (f, ...args) => f.bind(null, ...args)
+
+const _pipe = (f, g) => (...args) => g(f(...args))
+
+export const pipe = (...funcs) => funcs.reduce(_pipe)
