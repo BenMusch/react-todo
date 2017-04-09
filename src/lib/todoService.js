3a3152b6122b89fc61b263a389806ecaff38069b
Ben Muschol <benmuschol@gmail.com>
2017-04-09 10:12:27 -0400

    lesson 23: update todos

--- src/lib/todoService.js
+++ src/lib/todoService.js
@@ -5,6 +5,17 @@ export const loadTodos = () => {
     .then(response => response.json())
 }
 
+export const saveTodo = (todo) => {
+  return fetch(`${baseUrl}/${todo.id}`, {
+    method: 'PUT',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(todo)
+  }).then(response => response.json())
+}
+
 export const createTodo = (todo) => {
   return fetch(baseUrl, {
     method: 'POST',
